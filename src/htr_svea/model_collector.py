d0ccd4218202da0ae843eee39b325ee867a144fa
borg93 <gabriel.borg93@gmail.com>
2023-10-10 10:37:54 +0200

    updated toml and model_collector

--- src/htr_svea/model_collector.py
+++ src/htr_svea/model_collector.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 
 import torch
 from huggingface_hub import hf_hub_download
@@ -7,13 +8,21 @@ from mmdet.apis import DetInferencer
 from mmengine.config import Config
 from mmocr.apis import TextRecInferencer
 
-LIB_TEMP_NAME = "HTRansform"
-REPO_TYPE = "model"
-MODEL_FILE = "model.pth"
-CONFIG_FILE = "config.py"
+
+class OpenmmlabsFile(Enum):
+    MODEL_FILE = "model.pth"
+    CONFIG_FILE = "config.py"
+    DICT_FILE = "dictionary.txt"
+
+
+class OpenmmlabsFramework(Enum):
+    MMDET = "mmdet"
+    MMOCR = "mmocr"
 
 
 class OpenmmlabModel:
+    REPO_TYPE = "model"
+
     def __init__(self, config_files, model_files):
         self.config_files = config_files
         self.model_files = model_files
@@ -22,53 +31,83 @@ class OpenmmlabModel:
     @classmethod
     def from_pretrained(cls, model_id: str, cache_dir: str = None, device: str = None):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        model_files, config_files = cls.download_files(model_id, cache_dir)
-        if model_files and config_files:
-            cfg = Config.fromfile(config_files)
-            model = ModelFactory.create_model(cfg, config_files, model_files, device)
+
+        model_file, config_file = cls.download_config_and_model_file(model_id, cache_dir)
+
+        if model_file and config_file:
+            model_scope = cls._checking_model_scope(model_id, cache_dir, config_file)
+
+            model = OpenModelFactory.create_openmmlab_model(model_scope, config_file, model_file, device)
             return model
         return None
 
+    @classmethod
+    def _checking_model_scope(cls, model_id, cache_dir, config_file):
+        cfg = Config.fromfile(config_file)
+        model_scope = cfg.default_scope
+
+        if model_scope == OpenmmlabsFramework.MMOCR.value:
+            download_dict_file = cls.download_config_and_model_file(model_id, cache_dir)
+            cfg.dictionary["dict_file"] = download_dict_file
+
+            cfg.dump(config_file)
+        return model_scope
+
     def from_local(self):
         cfg = Config.fromfile(self.config_files)
-        model = ModelFactory.create_model(cfg, self.config_files, self.model_files, self.device)
+        model = OpenModelFactory.create_openmmlab_model(cfg, self.config_files, self.model_files, self.device)
         return model
 
     @staticmethod
-    def download_files(repo_id, cache_dir):
+    def download_config_and_model_file(repo_id, cache_dir):
         try:
-            model_files = hf_hub_download(
+            model_file = hf_hub_download(
                 repo_id=repo_id,
-                repo_type=REPO_TYPE,
-                filename=MODEL_FILE,
-                library_name=LIB_TEMP_NAME,
+                repo_type=OpenmmlabModel.REPO_TYPE,
+                filename=OpenmmlabsFile.MODEL_FILE.value,
+                library_name=__package__,
                 cache_dir=cache_dir,
             )
-            config_files = hf_hub_download(
+            config_file = hf_hub_download(
                 repo_id=repo_id,
-                repo_type=REPO_TYPE,
-                filename=CONFIG_FILE,
-                library_name=LIB_TEMP_NAME,
+                repo_type=OpenmmlabModel.REPO_TYPE,
+                filename=OpenmmlabsFile.CONFIG_FILE.value,
+                library_name=__package__,
                 cache_dir=cache_dir,
             )
-            return model_files, config_files
+            return model_file, config_file
+
         except RepositoryNotFoundError as e:
             logging.error(f"Could not download files for {repo_id}: {str(e)}")
             return None, None
 
-
-class ModelFactory:
     @staticmethod
-    def create_model(cfg, config_files, model_files, device):
+    def download_dict_file(repo_id, cache_dir):
+        try:
+            dictionary_file = hf_hub_download(
+                repo_id=repo_id,
+                repo_type=OpenmmlabModel.REPO_TYPE,
+                filename=OpenmmlabsFile.DICT_FILE.value,
+                library_name=__package__,
+                cache_dir=cache_dir,
+            )
+
+            return dictionary_file
+
+        except RepositoryNotFoundError as e:
+            logging.error(f"Could not download files for {repo_id}: {str(e)}")
+            return None
+
+
+class OpenModelFactory:
+    @staticmethod
+    def create_openmmlab_model(model_scope, config_files, model_files, device):
         model_creators = {
-            "mmdet": DetInferencer,
-            "mmocr": TextRecInferencer,
+            OpenmmlabsFramework.MMDET.value: DetInferencer,
+            OpenmmlabsFramework.MMOCR.value: TextRecInferencer,
         }
-        model_scope = cfg.default_scope
+
         if model_scope in model_creators:
             return model_creators[model_scope](config_files, model_files, device=device)
         logging.error(f"Unknown model scope: {model_scope}")
         return None
-
-
-# TODO  read config and update the cfg.dict path (i.e dump a new file and init the textrecinferencer based on the new file.)
